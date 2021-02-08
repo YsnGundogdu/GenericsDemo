2343a7f168db554b4d9b82785c4da3bb9976486c
yasin gündoğdu <ysn_gundogdu@hotmail.com>
2021-02-08 19:46:00 +0300

    Add project files.

---
+++ GenericsDemo/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDemo
+{
+    class MyDictionary<K,V>
+    {
+        K[]  studentNumber;
+        V[]  studentName;
+
+        public MyDictionary()
+        {
+            studentNumber = new K[0];
+            studentName = new V[0];
+
+        }
+
+        public void Add(K item1,V item2)
+        {
+            K[] tempKeyArray = studentNumber;
+            V[] tempValueArrat = studentName;
+
+            studentNumber = new K[studentNumber.Length + 1];
+            studentName = new V[studentName.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                studentNumber[i] = tempKeyArray[i];
+                studentName[i] = tempValueArrat[i];
+
+            }
+
+            studentNumber[studentNumber.Length - 1] = item1;
+            studentName[studentName.Length - 1] = item2;
+
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < studentName.Length; i++)
+            {
+                Console.WriteLine(studentNumber[i] + " no'lu öğrencimiz: " + studentName[i]);
+
+            }
+        }
+    }
+}
---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(394, "Yasin Gündoğdu");
+            myDictionary.Add(1782, "Yusuf Yetişkin");
+            myDictionary.Add(2284, "Oğuzhan Yıldız");
+            myDictionary.Add(1589, "İlker korkmaz");
+
+            myDictionary.List();
+        }
+    }
+}
